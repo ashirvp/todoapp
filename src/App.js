5f54c72422bb9dbaa679d98ca9ac6d73d574ac56
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-11-10 22:00:40 +0530

    commit changed

--- src/App.js
+++ src/App.js
@@ -15,23 +15,41 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => SetTodos([...toDos, toDo])} className="fas fa-plus"></i>
+        <i onClick={() => SetTodos([...toDos,{id:Date.now(),text:toDo,state:false} ])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        {toDos.map((value) => {
+        {toDos.map((obj) => {
 
 return(
           <div className="todo">
             <div className="left">
-              <input type="checkbox" name="" id="" />
-              <p>{value}</p>
+              <input onChange={(e)=>{
+                console.log(e.target.checked)
+                console.log(obj)
+                SetTodos(toDos.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj2.status=e.target.checked
+                  }
+                  return obj2
+                }))
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
             </div>
             <div className="right">
               <i className="fas fa-times"></i>
             </div>
           </div>
           )
-        })
+        }
+        )
+        }
+        {
+          toDos.map((obj)=>{
+            if(obj.status){
+              return(<h1>{obj.text}</h1>)
+            }
+            return null
+          })
         }
       </div>
     </div>
